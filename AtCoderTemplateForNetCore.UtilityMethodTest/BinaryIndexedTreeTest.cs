ca60351d4290066e9aae843a3846ef1ba23746d5
terry_u16 <terry-u16@users.noreply.github.com>
2020-05-22 17:25:29 +0900

    Add BinaryIndexedTree class

---
+++ AtCoderTemplateForNetCore.UtilityMethodTest/BinaryIndexedTreeTest.cs
@@ -0,0 +1,105 @@
+﻿using AtCoderTemplateForNetCore.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AtCoderTemplateForNetCore.UtilityMethodTest
+{
+    public class BinaryIndexedTreeTest
+    {
+        [Fact]
+        public void BITInitializationZeroTest()
+        {
+            const int length = 5;
+            var bit = new BinaryIndexedTree(length);
+            Assert.Equal(length, bit.Length);
+            Assert.Equal(0, bit.Sum(length));
+        }
+
+        [Fact]
+        public void BITSumFromZeroTest()
+        {
+            var a = new long[] { 5, 3, 7, 9, 8 };
+            long[] sum = GetPrefixSum(a);
+
+            var bit = new BinaryIndexedTree(a);
+
+            Assert.Equal(a.Length, bit.Length);
+            for (int i = 0; i < sum.Length; i++)
+            {
+                Assert.Equal(sum[i], bit.Sum(i));
+            }
+        }
+
+        [Fact]
+        public void BITAddTest()
+        {
+            var a = new long[] { 5, 3, 7, 9, 8 };
+            var sum = GetPrefixSum(a);
+
+            var bit = new BinaryIndexedTree(a);
+
+            Assert.Equal(a.Length, bit.Length);
+            for (int i = 0; i < sum.Length; i++)
+            {
+                Assert.Equal(sum[i], bit.Sum(i));
+            }
+
+            bit.AddAt(2, 3);
+            a[2] += 3;
+            sum = GetPrefixSum(a);
+
+            Assert.Equal(a.Length, bit.Length);
+            for (int i = 0; i < sum.Length; i++)
+            {
+                Assert.Equal(sum[i], bit.Sum(i));
+            }
+        }
+
+        [Fact]
+        public void BITSumFromAnyTest()
+        {
+            var a = new long[] { 5, 3, 7, 9, 8 };
+            var sum = GetPrefixSum(a);
+
+            var bit = new BinaryIndexedTree(a);
+
+            Assert.Equal(a.Length, bit.Length);
+            for (int begin = 0; begin < bit.Length; begin++)
+            {
+                for (int end = begin; end <= bit.Length; end++)
+                {
+                    Assert.Equal(sum[end] - sum[begin], bit.Sum(begin, end));
+                }
+            }
+        }
+
+        [Fact]
+        public void BITLowerBoundTest()
+        {
+            var a = new long[] { 5, 3, 7, 9, 8 };
+            var sum = GetPrefixSum(a);
+
+            var bit = new BinaryIndexedTree(a);
+
+            Assert.Equal(0, bit.GetLowerBound(0));
+            Assert.Equal(0, bit.GetLowerBound(3));
+            Assert.Equal(1, bit.GetLowerBound(8));
+            Assert.Equal(2, bit.GetLowerBound(9));
+            Assert.Equal(3, bit.GetLowerBound(18));
+            Assert.Equal(5, bit.GetLowerBound(100));
+        }
+
+        private static long[] GetPrefixSum(long[] a)
+        {
+            var sum = new long[a.Length + 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum[i + 1] = sum[i] + a[i];
+            }
+
+            return sum;
+        }
+    }
+}
--- AtCoderTemplateForNetCore/Program.cs
+++ AtCoderTemplateForNetCore/Program.cs
@@ -933,6 +933,108 @@ namespace AtCoderTemplateForNetCore.Collections
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
+    public class BinaryIndexedTree
+    {
+        long[] _data;
+        public ReadOnlySpan<long> Data => _data[1..];
+        public int Length { get; }
+
+        public BinaryIndexedTree(int length)
+        {
+            _data = new long[length + 1];   // 内部的には1-indexedにする
+            Length = length;
+        }
+
+        public BinaryIndexedTree(IEnumerable<long> data, int length) : this(length)
+        {
+            var count = 0;
+            foreach (var n in data)
+            {
+                AddAt(count++, n);
+            }
+        }
+
+        public BinaryIndexedTree(ICollection<long> collection) : this(collection, collection.Count) { }
+
+        /// <summary>
+        /// BITの<c>index</c>番目の要素に<c>n</c>を加算します。
+        /// </summary>
+        /// <param name="index">加算するインデックス（0-indexed）</param>
+        /// <param name="n">加算する数</param>
+        public void AddAt(Index index, long n)
+        {
+            var i = index.GetOffset(Length) + 1;  // 1-indexedにする
+            while (i <= Length)
+            {
+                _data[i] += n;
+                i += i & -i;    // LSBの加算
+            }
+        }
+
+        /// <summary>
+        /// [0, <c>end</c>)の部分和を返します。
+        /// </summary>
+        /// <param name="end">部分和を求める半開区間の終了インデックス</param>
+        /// <returns>区間の部分和</returns>
+        public long Sum(Index end)
+        {
+            var i = end.GetOffset(Length);  // 0-indexedの半開区間＝1-indexedの閉区間なので+1は不要
+            long sum = 0;
+            while (i > 0)
+            {
+                sum += _data[i];
+                i -= i & -i;    // LSBの減算
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// <c>range</c>の部分和を返します。
+        /// </summary>
+        /// <param name="range">部分和を求める半開区間</param>
+        /// <returns>区間の部分和</returns>
+        public long Sum(Range range) => Sum(range.End) - Sum(range.Start);
+
+        /// <summary>
+        /// [<c>start</c>, <c>end</c>)の部分和を返します。
+        /// </summary>
+        /// <param name="start">部分和を求める半開区間の開始インデックス</param>
+        /// <param name="end">部分和を求める半開区間の終了インデックス</param>
+        /// <returns>区間の部分和</returns>
+        public long Sum(int start, int end) => Sum(end) - Sum(start);
+
+        /// <summary>
+        /// [0, <c>index</c>)の部分和が<c>sum</c>未満になる最大の<c>index</c>を返します。
+        /// BIT上の各要素は0以上の数である必要があります。
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        public int GetLowerBound(long sum)
+        {
+            int index = 0;
+            for (int offset = GetMostSignificantBitOf(Length); offset > 0; offset >>= 1)
+            {
+                if (index + offset < _data.Length && _data[index + offset] < sum)
+                {
+                    index += offset;
+                    sum -= _data[index];
+                }
+            }
+
+            return index;
+
+            int GetMostSignificantBitOf(int n)
+            {
+                int k = 1;
+                while ((k << 1) <= n)
+                {
+                    k <<= 1;
+                };
+                return k;
+            }
+        }
+    }
+
     public class Counter<T> : IEnumerable<(T key, long count)> where T : IEquatable<T>
     {
         private Dictionary<T, long> _innerDictionary;
