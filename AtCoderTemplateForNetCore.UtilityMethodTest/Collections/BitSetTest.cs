6182c3a90611e9e2cffb65ce089d4dd26b4f84b9
terry_u16 <terry-u16@users.noreply.github.com>
2020-05-24 19:46:07 +0900

    Add BitSet structure

---
+++ AtCoderTemplateForNetCore.UtilityMethodTest/Collections/BitSetTest.cs
@@ -0,0 +1,54 @@
+﻿using AtCoderTemplateForNetCore.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AtCoderTemplateForNetCore.UtilityMethodTest.Collections
+{
+    public class BitSetTest
+    {
+        [Theory]
+        [InlineData(0x0f0f0f0f, 0xf0f0f0f0)]
+        [InlineData(0x1, 0x80000000)]
+        public void ReverseTest(uint input, uint output)
+        {
+            Assert.Equal(output, new BitSet(input).Reverse());
+        }
+
+        [Theory]
+        [InlineData(0x0f0f0f0f, 16)]
+        [InlineData(0x0, 0)]
+        public void PopCountTest(uint input, int count)
+        {
+            Assert.Equal(count, new BitSet(input).Count());
+        }
+
+        [Theory]
+        [InlineData(0xf0, 0x10)]
+        public void LsbTest(uint input, uint lsb)
+        {
+            Assert.Equal(lsb, new BitSet(input).Lsb());
+        }
+
+        [Fact]
+        public void IndexerTest()
+        {
+            var bitSet = new BitSet(0b0101);
+            Assert.True(bitSet[0]);
+            Assert.False(bitSet[1]);
+            Assert.True(bitSet[2]);
+            Assert.False(bitSet[3]);
+        }
+
+        [Theory]
+        [InlineData(0b0001, 2, true, 0b0101)]
+        [InlineData(0b0001, 2, false, 0b0001)]
+        [InlineData(0b0001, 0, true, 0b0001)]
+        [InlineData(0b0001, 0, false, 0b0000)]
+        public void SetAtTest(uint input, int digit, bool value, uint expected)
+        {
+            Assert.Equal(expected, new BitSet(input).SetAt(digit, value));
+        }
+    }
+}
--- AtCoderTemplateForNetCore/Program.cs
+++ AtCoderTemplateForNetCore/Program.cs
@@ -1321,6 +1321,72 @@ namespace AtCoderTemplateForNetCore.Collections
         }
     }
 
+    public readonly struct BitSet : IEquatable<BitSet>
+    {
+        readonly uint _value;
+
+        public BitSet(uint value)
+        {
+            _value = value;
+        }
+        public bool this[int digit]
+        {
+            get => ((_value >> digit) & 1) > 0;
+        }
+        public bool Any => _value > 0;
+        public bool None => _value == 0;
+        public BitSet SetAt(int digit, bool value) => value ? new BitSet(_value | (1u << digit)) : new BitSet(_value & ~(1u << digit));
+        public BitSet Lsb() { unchecked { return new BitSet(_value & (uint)-(int)_value); } }
+        public BitSet Reverse()
+        {
+            unchecked
+            {
+                uint v = _value;
+                v = (v & 0x55555555) << 1 | (v >> 1 & 0x55555555);
+                v = (v & 0x33333333) << 2 | (v >> 2 & 0x33333333);
+                v = (v & 0x0f0f0f0f) << 4 | (v >> 4 & 0x0f0f0f0f);
+                v = (v & 0x00ff00ff) << 8 | (v >> 8 & 0x00ff00ff);
+                v = (v & 0x0000ffff) << 16 | (v >> 16 & 0x0000ffff);
+                return new BitSet(v);
+            }
+        }
+        public int Count()
+        {
+            unchecked
+            {
+                // Hardware Intrinsics未使用
+                uint v = _value;
+                v = (v & 0x55555555) + (v >> 1 & 0x55555555);
+                v = (v & 0x33333333) + (v >> 2 & 0x33333333);
+                v = (v & 0x0f0f0f0f) + (v >> 4 & 0x0f0f0f0f);
+                v = (v & 0x00ff00ff) + (v >> 8 & 0x00ff00ff);
+                v = (v & 0x0000ffff) + (v >> 16 & 0x0000ffff);
+                return (int)v;
+            }
+        }
+
+        public static BitSet Zero => new BitSet(0);
+        public static BitSet One => new BitSet(1);
+        public static BitSet All => new BitSet(~0u);
+        public static BitSet CreateMask(int digit) => new BitSet((1u << digit) - 1);
+        public static BitSet operator ++(BitSet bitSet) => new BitSet(bitSet._value + 1);
+        public static BitSet operator --(BitSet bitSet) => new BitSet(bitSet._value - 1);
+        public static BitSet operator ~(BitSet bitSet) => new BitSet(~bitSet._value);
+        public static BitSet operator &(BitSet left, BitSet right) => new BitSet(left._value & right._value);
+        public static BitSet operator |(BitSet left, BitSet right) => new BitSet(left._value | right._value);
+        public static BitSet operator ^(BitSet left, BitSet right) => new BitSet(left._value ^ right._value);
+        public static BitSet operator <<(BitSet bitSet, int n) => new BitSet(bitSet._value << n);
+        public static BitSet operator >>(BitSet bitSet, int n) => new BitSet(bitSet._value >> n);
+        public static bool operator ==(BitSet left, BitSet right) => left.Equals(right);
+        public static bool operator !=(BitSet left, BitSet right) => !(left == right);
+        public static implicit operator uint(BitSet bitSet) => bitSet._value;
+
+        public override bool Equals(object obj) => obj is BitSet set && Equals(set);
+        public bool Equals(BitSet other) => _value == other._value;
+        public override string ToString() => Convert.ToString(_value, 2);
+        public override int GetHashCode() => _value.GetHashCode();
+    }
+
     public static class SearchExtensions
     {
         public static Range GetRangeGreaterEqual<T>(this Span<T> span, T minValue) where T : IComparable<T> => GetRangeGreaterEqual((ReadOnlySpan<T>)span, minValue);
