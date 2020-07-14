dbc598d07aba2980a8a6ad686a0b49d93375529f
terry_u16 <terry-u16@users.noreply.github.com>
2020-07-14 20:43:42 +0900

    Add Deque<T> class

---
+++ AtCoderTemplateForNetCore.UtilityMethodTest/Collections/DequeTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AtCoderTemplateForNetCore.Collections;
+using Xunit;
+
+namespace AtCoderTemplateForNetCore.UtilityMethodTest.Collections
+{
+    public class DequeTest
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(1000000)]
+        public void InitializeWithCapacityTest(int n)
+        {
+            var deque = new Deque<int>(n);
+        }
+
+        [Fact]
+        public void InitializeWithCollectionTest()
+        {
+            const int N = 100000;
+            var deque = new Deque<int>(Enumerable.Range(0, N));
+            var count = 0;
+
+            Assert.Equal(N, deque.Count);
+
+            foreach (var item in deque)
+            {
+                Assert.Equal(count++, item);
+            }
+        }
+
+        [Fact]
+        public void EnqueueFirstTest()
+        {
+            var deque = new Deque<int>();
+            foreach (var value in Enumerable.Range(0, 10000))
+            {
+                deque.EnqueueFirst(value);
+            }
+
+            var expected = 9999;
+            foreach (var item in deque)
+            {
+                Assert.Equal(expected--, item);
+            }
+        }
+
+        [Fact]
+        public void EnqueueLastTest()
+        {
+            var deque = new Deque<int>();
+            foreach (var value in Enumerable.Range(0, 10000))
+            {
+                deque.EnqueueLast(value);
+            }
+
+            var expected = 0;
+            foreach (var item in deque)
+            {
+                Assert.Equal(expected++, item);
+            }
+        }
+
+        [Fact]
+        public void DequeueFirstTest()
+        {
+            var deque = new Deque<int>();
+            var stack = new Stack<int>();
+            foreach (var value in Enumerable.Range(0, 10000))
+            {
+                deque.EnqueueFirst(value);
+                stack.Push(value);
+            }
+
+            foreach (var value in stack)
+            {
+                Assert.Equal(value, deque.DequeueFirst());
+            }
+        }
+
+        [Fact]
+        public void DequeueLastTest()
+        {
+            var deque = new Deque<int>();
+            var stack = new Stack<int>();
+            foreach (var value in Enumerable.Range(0, 10000))
+            {
+                deque.EnqueueLast(value);
+                stack.Push(value);
+            }
+
+            foreach (var value in stack)
+            {
+                Assert.Equal(value, deque.DequeueLast());
+            }
+        }
+
+        [Fact]
+        public void IndexerTest()
+        {
+            const int N = 7;
+            var deque = new Deque<int>();
+            foreach (var value in Enumerable.Range(0, N))
+            {
+                deque.EnqueueLast(value);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Assert.Equal(i, deque[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Assert.Equal(N - 1 - i, deque[^(i + 1)]);
+            }
+        }
+
+        [Fact]
+        public void DequeueWhenCountIsZeroThrowsExceptionTest()
+        {
+            var deque = new Deque<int>();
+            Assert.Throws<InvalidOperationException>(() => deque.DequeueFirst());
+            Assert.Throws<InvalidOperationException>(() => deque.DequeueLast());
+        }
+    }
+}
--- AtCoderTemplateForNetCore/Program.cs
+++ AtCoderTemplateForNetCore/Program.cs
@@ -981,6 +981,143 @@ namespace AtCoderTemplateForNetCore.Collections
         }
     }
 
+    public class Deque<T> : IReadOnlyCollection<T>
+    {
+        public int Count { get; private set; }
+        private T[] _data;
+        private int _first;
+        private int _mask;
+
+        public Deque() : this(4) { }
+
+        public Deque(int minCapacity)
+        {
+            if (minCapacity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minCapacity), $"{nameof(minCapacity)}は0より大きい値でなければなりません。");
+            }
+            var capacity = GetPow2Over(minCapacity);
+            _data = new T[capacity];
+            _first = 0;
+            _mask = capacity - 1;
+        }
+
+        public Deque(IEnumerable<T> collection)
+        {
+            var dataArray = collection.ToArray();
+            var capacity = GetPow2Over(dataArray.Length);
+            _data = new T[capacity];
+            _first = 0;
+            _mask = capacity - 1;
+
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                _data[i] = dataArray[i];
+                Count++;
+            }
+        }
+
+        public T this[Index index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                var offset = index.GetOffset(Count);
+                return _data[(_first + offset) & _mask];
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void EnqueueFirst(T item)
+        {
+            if (_data.Length == Count)
+            {
+                Resize();
+            }
+
+            _first = (_first - 1) & _mask;
+            _data[_first] = item;
+            Count++;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void EnqueueLast(T item)
+        {
+            if (_data.Length == Count)
+            {
+                Resize();
+            }
+
+            _data[(_first + Count++) & _mask] = item;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T DequeueFirst()
+        {
+            if (Count == 0)
+            {
+                ThrowInvalidOperationException("Queueが空です。");
+            }
+
+            var value = _data[_first];
+            _data[_first++] = default;
+            _first &= _mask;
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T DequeueLast()
+        {
+            if (Count == 0)
+            {
+                ThrowInvalidOperationException("Queueが空です。");
+            }
+
+            var index = (_first + --Count) & _mask;
+            var value = _data[index];
+            _data[index] = default;
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Resize()
+        {
+            var newArray = new T[_data.Length << 1];
+            var span = _data.AsSpan();
+            var firstHalf = span[_first..];
+            var lastHalf = span[.._first];
+            firstHalf.CopyTo(newArray);
+            lastHalf.CopyTo(newArray.AsSpan(firstHalf.Length));
+            _data = newArray;
+            _first = 0;
+            _mask = _data.Length - 1;
+        }
+
+        private void ThrowInvalidOperationException(string message) => throw new InvalidOperationException(message);
+
+        private int GetPow2Over(int n)
+        {
+            n--;
+            var result = 1;
+            while (n != 0)
+            {
+                n >>= 1;
+                result <<= 1;
+            }
+            return result;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
     public class PriorityQueue<T> : IEnumerable<T> where T : IComparable<T>
     {
         private List<T> _heap = new List<T>();
