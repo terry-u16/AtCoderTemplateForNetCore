5c51888bff95adf9144dcd3827a35a0a18d44f70
terry_u16 <terry-u16@users.noreply.github.com>
2020-08-13 13:43:26 +0900

    Add XorShift

--- AtCoderTemplateForNetCore/Program.cs
+++ AtCoderTemplateForNetCore/Program.cs
@@ -945,6 +945,51 @@ namespace AtCoderTemplateForNetCore.Algorithms
         public override string ToString() => RawString;
     }
 
+    public class XorShift
+    {
+        ulong _x;
+
+        public XorShift() : this((ulong)DateTime.Now.Ticks) { }
+
+        public XorShift(ulong seed)
+        {
+            _x = seed;
+        }
+
+        /// <summary>
+        /// [0, (2^64)-1)の乱数を生成します。
+        /// </summary>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ulong Next()
+        {
+            _x = _x ^ (_x << 13);
+            _x = _x ^ (_x >> 7);
+            _x = _x ^ (_x << 17);
+            return _x;
+        }
+
+        /// <summary>
+        /// [0, <c>exclusiveMax</c>)の乱数を生成します。
+        /// </summary>
+        /// <param name="exclusiveMax"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Next(int exclusiveMax) => (int)(Next() % (uint)exclusiveMax);
+
+        /// <summary>
+        /// [0.0, 1.0)の乱数を生成します。
+        /// </summary>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public double NextDouble()
+        {
+            const ulong max = 1UL << 50;
+            const ulong mask = max - 1;
+            return (double)(Next() & mask) / max;
+        }
+    }
+
     public static class AlgorithmHelpers
     {
         public static void UpdateWhenSmall<T>(ref T value, T other) where T : IComparable<T>
