a5b9615d62c48fed99e0dbdb2f783d2ac22e4ff5
terry_u16 <terry-u16@users.noreply.github.com>
2020-10-31 14:51:58 +0900

    Add StressTester

---
+++ StressTester/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using AtCoderTemplateForNetCore;
+using AtCoderTemplateForNetCore.Algorithms;
+using AtCoderTemplateForNetCore.Questions;
+
+namespace StressTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAtCoderQuestion question = new QuestionA();
+            IAtCoderQuestion naiveSolver = new NaiveSolver();
+
+            long count = 0;
+
+            while (true)
+            {
+                using var input = new MemoryStream();
+                using var expectedOutput = new MemoryStream();
+                using var actualOutput = new MemoryStream();
+                var expectedIO = new IOManager(input, expectedOutput);
+                var actualIO = new IOManager(input, actualOutput);
+                var inputReader = new StreamReader(input);
+                var inputWriter = new StreamWriter(input);
+                var expectedOutputReader = new StreamReader(expectedOutput);
+                var actualOutputReader = new StreamReader(actualOutput);
+
+                // 入力作成
+                GenerateInput(inputWriter);
+                inputWriter.Flush();
+
+                // チェック
+                input.Position = 0;
+                naiveSolver.Solve(expectedIO);
+                expectedIO.Flush();
+                input.Position = 0;
+                question.Solve(actualIO);
+                actualIO.Flush();
+
+                input.Position = 0;
+                expectedOutput.Position = 0;
+                actualOutput.Position = 0;
+
+                Console.WriteLine($"[Test {count++}]");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("[Input]");
+                Console.ResetColor();
+                Console.Write(inputReader.ReadToEnd());
+
+                var expected = expectedOutputReader.ReadToEnd();
+                var actual = actualOutputReader.ReadToEnd();
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("[Expected]");
+                Console.ResetColor();
+                Console.Write(expected);
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("[Actual]");
+                Console.ResetColor();
+                Console.Write(actual);
+
+                if (expected == actual)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Accepted");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Wrong Answer");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+
+        private static void GenerateInput(StreamWriter writer)
+        {
+            var random = new XorShift();
+
+            // 入力を作成
+        }
+    }
+
+    class NaiveSolver : AtCoderQuestionBase
+    {
+        public override void Solve(IOManager io)
+        {
+            // 愚直解を記述
+        }
+    }
+}
